be3518bea9112f156b6a551b2d6a57b197892d2b
Thiago Barradas <thiagobrowskt@gmail.com>
2017-11-19 19:37:14 -0200

    :bug: fixes path for powershell script

--- pack_client.ps1
+++ pack_client.ps1
@@ -1,11 +1,12 @@
 # reference: http://www.codeproject.com/Tips/806257/Automating-NuGet-Package-Creation-using-AppVeyor
 $root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
-$version = [System.Reflection.Assembly]::LoadFile("UAUtil\bin\Release\netstandard2.0\UAUtil.dll").GetName().Version
+echo '$root'
+$version = [System.Reflection.Assembly]::LoadFile("$root\UAUtil\bin\Release\netstandard2.0\UAUtil.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
 
-$content = (Get-Content UAUtil\UAUtil.nuspec) 
+$content = (Get-Content $root\UAUtil\UAUtil.nuspec) 
 $content = $content -replace '\$version\$',$versionStr
 
 $content | Out-File $root\UAUtil.compiled.nuspec
