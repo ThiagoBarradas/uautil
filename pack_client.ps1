d00893bb9ce4308682d38d18fbcfff92accc1a6e
Thiago Barradas <thiagobrowskt@gmail.com>
2017-11-19 19:39:46 -0200

    :bug: fixes path for powershell script

--- pack_client.ps1
+++ pack_client.ps1
@@ -1,6 +1,6 @@
 # reference: http://www.codeproject.com/Tips/806257/Automating-NuGet-Package-Creation-using-AppVeyor
-$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
-echo $root
+$root = (split-path -parent $MyInvocation.MyCommand.Definition)
+
 $version = [System.Reflection.Assembly]::LoadFile("$root\UAUtil\bin\Release\netstandard2.0\UAUtil.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
